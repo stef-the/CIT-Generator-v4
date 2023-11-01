797dda416702e7d4abb7a110aced3f01abc33a0a
stef <stefthedoggo4@gmail.com>
2023-11-01 09:21:48 +0000

    switch loader fully to python

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,51 +1,67 @@
 import json
 import time
 from os import listdir
-from os.path import isfile, join
+import os.path
 
-start_time = time.time()  # Get starting time
-print("STARTING DUPLICATES\n--- %s seconds ---" % (time.time() - start_time))
 
-items_path = "src/data/items/"
+def main():
+    start_time = time.time()  # Get starting time
+    print("STARTING DUPLICATES\n--- %s seconds ---" % (time.time() - start_time))
 
-files = [f for f in listdir(items_path) if isfile(join(items_path, f))]
-file_names = []
-duplicates_output = {}
+    items_path = "src/data/items/"
 
-for file in files:
-    with open(items_path + file, encoding="utf-8") as json_data:
-        file_names.append(json.load(json_data)["displayname"].split("§")[-1])
+    files = [
+        f for f in listdir(items_path) if os.path.isfile(os.path.join(items_path, f))
+    ]
+    file_names = []
+    duplicates_output = {}
 
-duplicates = []
-for i in file_names:
-    if not i in duplicates:
-        increment = 0
-        for j in file_names:
-            if i == j:
-                increment += 1
-                if increment > 1:
-                    duplicates.append(i)
-                    break
+    for file in files:
+        with open(items_path + file, encoding="utf-8") as json_data:
+            file_names.append(json.load(json_data)["displayname"].split("§")[-1])
 
-duplicates_output["duplicates"] = duplicates
-duplicates_output["count"] = len(duplicates)
-duplicates_output["timestamp"] = time.time()
+    duplicates = []
+    for i in file_names:
+        if not i in duplicates:
+            increment = 0
+            for j in file_names:
+                if i == j:
+                    increment += 1
+                    if increment > 1:
+                        duplicates.append(i)
+                        break
 
-with open("src/data/duplicates.json", "w") as outfile:
-    json.dump(duplicates_output, outfile)
+    duplicates_output["duplicates"] = duplicates
+    duplicates_output["count"] = len(duplicates)
+    duplicates_output["timestamp"] = time.time()
 
-print("DUPLICATES COMPLETE\n--- %s seconds ---" % (time.time() - start_time))
+    with open("src/data/duplicates.json", "w") as outfile:
+        json.dump(duplicates_output, outfile)
 
-print("STARTING OUTPUT CONFIG\n--- %s seconds ---" % (time.time() - start_time))
+    print("DUPLICATES COMPLETE\n--- %s seconds ---" % (time.time() - start_time))
 
-internal_names = [x.split(".")[0] for x in files]
+    print("STARTING OUTPUT CONFIG\n--- %s seconds ---" % (time.time() - start_time))
 
-# Output should look like this:
-# { internal_name_a, internal_name_b, internal_name_c, ... }
+    fileList = []
+    path = "src/data/items"
+    for filenames in os.walk(path):
+        fileList.append(filenames)
 
-import_set = set()
-for i in internal_names:
-    import_set.add(i)
+    data_list = {}
 
-with open("src/data/index.js", "w") as outfile:
-    outfile.write(f'import f{import_set} from "./items/"')
+    for i in fileList[0][2]:
+        try:
+            data = json.load(open(path + "/" + i, encoding="utf-8"))
+            data_list[i.split(".")[0]] = data
+        except Exception as e:
+            print(e)
+
+    out_file = open("src/data/items.json", "w")
+    json.dump(data_list, out_file, indent=4)
+    out_file.close()
+
+    print("OUTPUT CONFIG COMPLETE\n--- %s seconds ---" % (time.time() - start_time))
+
+
+if __name__ == "__main__":
+    main()
